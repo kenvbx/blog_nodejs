7bebb52cb9adffad2c5338861a08b0c1b192a0a0
Tuyen Pham <pthanhtuyen2411@gmail.com>
2021-04-25 11:29:18 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
 
 const route = require('./routes');
 
@@ -29,11 +33,11 @@ app.engine(
 );
 
 app.set("view engine", 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
